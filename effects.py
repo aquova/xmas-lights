7b31b0456e3e5ce8106b1f351aab6e14bf6cbce3
aquova <abrick912@gmail.com>
2021-12-04 15:47:54 -0600

    Add randomness to rainbow effect

--- effects.py
+++ effects.py
@@ -1,18 +1,20 @@
 import neopixel
-import math, time
+from math import ceil, floor
+from random import randint
+from time import sleep
 
 NUM_BULBS = 150
 RGB_SIZE = 255 * 6
-STEP_SIZE = math.floor(RGB_SIZE / NUM_BULBS)
+STEP_SIZE = floor(RGB_SIZE / NUM_BULBS)
 
 def clear(pixels):
     pixels.fill((0, 0, 0))
 
 def rainbow_effect(pixels):
+    offset = randint(0, NUM_BULBS - 1)
     for i, (r, g, b) in enumerate(rainbow_gradient()):
-        if i == NUM_BULBS:
-            break
-        pixels[i] = (r, g, b)
+        ii = (i + offset) % NUM_BULBS
+        pixels[ii] = (r, g, b)
 
 def christmas_effect(pixels):
     pixels.fill((255, 0, 0))
@@ -35,18 +37,17 @@ def cycle_effect(pixels):
     while True:
         for (r, g, b) in rainbow_gradient():
             pixels.fill((r, g, b))
-            time.sleep(0.5)
+            sleep(0.5)
 
 def mountain_effect(pixels):
     PEAKS = [0, 6, 36, 61, 96, 129, 149]
 
-    # top_color = (255, 255, 255)
-    top_color = (0, 255, 0)
-    btm_color = (255, 0, 0)
+    top_color = (244, 186, 253)
+    btm_color = (96, 1, 112)
     for idx in range(len(PEAKS) - 1):
         left = PEAKS[idx]
         right = PEAKS[idx + 1]
-        gradient = color_gradient(top_color, btm_color, math.ceil((right - left) / 2))
+        gradient = color_gradient(top_color, btm_color, ceil((right - left) / 2) + 1)
         for offset, color in enumerate(gradient):
             pixels[left + offset] = color
             pixels[right - offset] = color
@@ -57,9 +58,9 @@ def color_gradient(start, end, num):
     bm = start[2] - end[2]
 
     for i in range(num):
-        red = start[0] - math.floor(rm * i / num)
-        green = start[1] - math.floor(gm * i / num)
-        blue = start[2] - math.floor(bm * i / num)
+        red = start[0] - floor(rm * i / num)
+        green = start[1] - floor(gm * i / num)
+        blue = start[2] - floor(bm * i / num)
         yield red, green, blue
 
 def rainbow_gradient():
